742091a10bb7ef470749f1a892ecf58ec86ab260
Aayush3014 <ayushak3008@gmail.com>
2023-03-08 21:24:19 +0530

    Code for Other Features

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -7,13 +7,18 @@ from django.shortcuts import render
 def index(request):
 
     return render(request,'index.html')
-    # return HttpResponse("<h1>Home</h1>")
 
 def analyze(request):
+    # Get the text
     djtext = request.GET.get('text','default')
+
+    # Check values for checkbox.
     removepunc = request.GET.get('removepunc','off')
-    print(removepunc)
-    print(djtext)
+    fullcaps = request.GET.get('fullcaps','off')
+    newlineremover = request.GET.get('newlineremover','off')
+    spaceremover = request.GET.get('spaceremover','off')
+
+    # if removepunc checkbox is on
     if removepunc == "on":
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
@@ -22,7 +27,32 @@ def analyze(request):
                 analyzed = analyzed + char
         params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
         return render(request, 'analyze.html', params)
-
+    
+    # if fullcaps checkbox is on
+    elif (fullcaps=='on'):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    
+    # if newlineremover is on
+    elif (newlineremover=='on'):
+        analyzed = ""
+        for char in djtext:
+            if char != '\n':
+                analyzed = analyzed + char
+        params = {'purpose': 'New Lines Removed', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    
+    # If Space remover is on.
+    elif (spaceremover=='on'):
+        analyzed = ""
+        for char in djtext:
+            if char!=' ':
+                analyzed = analyzed + char
+        params = {'purpose': 'Spaces Removed', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
     else:
         return HttpResponse('Error')
 
